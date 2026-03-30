d80491714c6b001ece1ba0ff79a29078266c7578
hickokchrist <hickokchris@proton.me>
2026-03-30 09:55:15 -0700

    fixing CDN cache issue which should be the cause of the APOD on the homepage being frozen in time

--- src/pages/api/apod.js
+++ src/pages/api/apod.js
@@ -12,7 +12,7 @@ export async function GET({ request }) {
   return new Response(JSON.stringify(data), {
     headers: {
       'Content-Type': 'application/json',
-      'Cache-Control': 'public, max-age=86400'
+      'Cache-Control': 'public, max-age=86400, s-maxage=0'
     }
   });
 }
